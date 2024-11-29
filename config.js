7a8c0e771b743b85407ab113aa6c23bec6e113f8
danil <ap.fomin203@gmail.com>
2024-11-29 16:56:14 +0300

    add new stiker

--- config.js
+++ config.js
@@ -5,11 +5,16 @@ dotenv.config();
 export const configBot = {
   token: process.env.BOT_TOKEN,
   idSticker:
-    "CAACAgIAAxkBAAENOWtnRgvJ3XmMGgFW_j7PQV7zVHsY5AACNQEAAjDUnRG0uDX9ZqC2fDYE",
+    "CAACAgIAAxkBAAENO-tnSARACPQfihnQ0JF70kNdscKtOwACzTIAAtyEWEgs4kVS4Lfk0DYE",
   options: {
     reply_markup: JSON.stringify({
       inline_keyboard: [
-        [{ text: "Пройти тест", web_app: { url: "https://hackathon-front-rho.vercel.app/" } }],
+        [
+          {
+            text: "Пройти тест",
+            web_app: { url: "https://hackathon-front-rho.vercel.app/" },
+          },
+        ],
       ],
     }),
   },
--- index.js
+++ index.js
@@ -1,31 +1,47 @@
 import TelegramBot from "node-telegram-bot-api";
+import express from "express";
+import { fileURLToPath } from "url";
+import path from "path";
 
 import { configBot } from "./config.js";
 
-const bot = new TelegramBot(configBot.token, {
-  polling: true,
-});
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-bot.on("message", async (msg) => {
-  const chatId = msg.chat.id;
-  const text = msg.text;
+const app = express();
+const port = 3000;
 
-  switch (text) {
-    case "/start":
-      await bot.sendSticker(chatId, configBot.idSticker);
-      await bot.sendMessage(chatId, "Привет, я бот Мавр и вот что я умею...");
-      await bot.sendMessage(
-        chatId,
-        "Здесь идёт описание теста..",
-        configBot.options
-      );
-      break;
+app.use(express.static(__dirname + "/public"));
 
-    default:
-      break;
-  }
-});
+app.listen(port, () => {
+  const bot = new TelegramBot(configBot.token, {
+    polling: true,
+  });
 
-bot.on("callback_query", (msg) => {
-  console.log(JSON.parse(JSON.stringify(msg.data)));
+  bot.on("message", async (msg) => {
+    const chatId = msg.chat.id;
+    const text = msg.text;
+
+    switch (text) {
+      case "/start":
+        await bot.sendSticker(chatId, configBot.idSticker);
+        await bot.sendMessage(
+          chatId,
+          "Приветствую тебя! Я — бот Мавр, и я здесь, чтобы помочь тебе совершить важный шаг на пути к твоему будущему. Выбор профессии — это не простое решение. Позволь мне помочь тебе с выбором будущей профессии!"
+        );
+        await bot.sendMessage(
+          chatId,
+          "Пройди наш увлекательный тест, чтобы я помог тебе определить твою будущую профессию!",
+          configBot.options
+        );
+        break;
+      default:
+        await bot.sendSticker(
+          chatId,
+          "CAACAgIAAxkBAAENPiVnSceUYaKk_CBMVBjIMSs-LntXAgACpTkAArTwWEjLlMMDTBAB8TYE"
+        );
+        await bot.sendMessage(chatId, "Я не знаю такую команду.");
+        break;
+    }
+  });
 });
