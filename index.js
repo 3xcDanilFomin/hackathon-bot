2d49bd1fb63aec7757e942e6d897941e4436461e
danil <ap.fomin203@gmail.com>
2024-11-29 21:57:55 +0300

    update port

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import TelegramBot from "node-telegram-bot-api";
 import express from "express";
+import cors from "cors";
 import { fileURLToPath } from "url";
 import path from "path";
 
@@ -9,39 +10,46 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const port = 3000;
+const port = 8080;
 
 app.use(express.static(__dirname + "/public"));
+const bot = new TelegramBot(configBot.token, {
+  polling: true,
+});
+app.use(express.json());
+app.use(cors());
+
+bot.on("message", async (msg) => {
+  const chatId = msg.chat.id;
+  const text = msg.text;
+
+  switch (text) {
+    case "/start":
+      await bot.sendSticker(chatId, configBot.idSticker);
+      await bot.sendMessage(
+        chatId,
+        "Приветствую тебя! Я — бот Мавр, и я здесь, чтобы помочь тебе совершить важный шаг на пути к твоему будущему. Выбор профессии — это не простое решение. Позволь мне помочь тебе с выбором будущей профессии!"
+      );
+      await bot.sendMessage(
+        chatId,
+        "Пройди наш увлекательный тест, чтобы я помог тебе определить твою будущую профессию!",
+        configBot.options
+      );
+      break;
+    default:
+      await bot.sendSticker(
+        chatId,
+        "CAACAgIAAxkBAAENPiVnSceUYaKk_CBMVBjIMSs-LntXAgACpTkAArTwWEjLlMMDTBAB8TYE"
+      );
+      await bot.sendMessage(chatId, "Я не знаю такую команду.");
+      break;
+  }
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  const bot = new TelegramBot(configBot.token, {
-    polling: true,
-  });
-
-  bot.on("message", async (msg) => {
-    const chatId = msg.chat.id;
-    const text = msg.text;
-
-    switch (text) {
-      case "/start":
-        await bot.sendSticker(chatId, configBot.idSticker);
-        await bot.sendMessage(
-          chatId,
-          "Приветствую тебя! Я — бот Мавр, и я здесь, чтобы помочь тебе совершить важный шаг на пути к твоему будущему. Выбор профессии — это не простое решение. Позволь мне помочь тебе с выбором будущей профессии!"
-        );
-        await bot.sendMessage(
-          chatId,
-          "Пройди наш увлекательный тест, чтобы я помог тебе определить твою будущую профессию!",
-          configBot.options
-        );
-        break;
-      default:
-        await bot.sendSticker(
-          chatId,
-          "CAACAgIAAxkBAAENPiVnSceUYaKk_CBMVBjIMSs-LntXAgACpTkAArTwWEjLlMMDTBAB8TYE"
-        );
-        await bot.sendMessage(chatId, "Я не знаю такую команду.");
-        break;
-    }
-  });
+  console.log("Сервер запущен на " + port);
 });
