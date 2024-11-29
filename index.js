7d72da4d3af244a5c3477bf6ae5c546e68c20a07
danil <ap.fomin203@gmail.com>
2024-11-30 01:11:49 +0300

    remove server

--- index.js
+++ index.js
@@ -1,30 +1,11 @@
 import TelegramBot from "node-telegram-bot-api";
-import express from "express";
-import cors from "cors";
-import { fileURLToPath } from "url";
-import path from "path";
-import https from "https"; 
-import fs from "fs"; 
-
 import { configBot } from "./config.js";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
-const app = express();
-const port = 8080;
-
-const options = {
-  key: fs.readFileSync("/root/mycerts/private.key"), 
-  cert: fs.readFileSync("/root/mycerts/certificate.crt"), 
-};
-
-app.use(express.static(__dirname + "/public"));
 const bot = new TelegramBot(configBot.token, {
   polling: true,
 });
-app.use(express.json());
-app.use(cors());
+
 
 bot.on("message", async (msg) => {
   const chatId = msg.chat.id;
@@ -51,12 +32,4 @@ bot.on("message", async (msg) => {
       await bot.sendMessage(chatId, "Я не знаю такую команду.");
       break;
   }
-});
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-https.createServer(options, app).listen(port, () => {
-  console.log("Сервер запущен на " + port);
 });
